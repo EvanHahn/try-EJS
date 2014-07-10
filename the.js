5ec615e48463e0d063449800d5e6cd51bdc0707c
Evan Hahn <me@evanhahn.com>
2014-07-10 13:52:24 -0700

    REAL initial commit

---
+++ the.js
@@ -0,0 +1,27 @@
+$(function() {
+
+  var $input = $("#template");
+  var $data = $("#data");
+  var $output = $("#output");
+
+  function render() {
+    var output;
+    try {
+      var src = $input.val();
+      var context = Function("", "return " + $data.val())() || {};
+      output = ejs.render(src, context);
+      $output.removeClass("error");
+    } catch (err) {
+      output = err;
+      $output.addClass("error");
+    }
+    $output.html(output);
+  }
+
+  var renderThrottled = $.throttle(250, render);
+  $input.on("keyup", renderThrottled);
+  $data.on("keyup", renderThrottled);
+
+  render();
+
+});
